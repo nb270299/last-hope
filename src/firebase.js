e1d3123d1ea68309178dc467348bbc600e78038e
nedibrsh <nedibreshani@hotmail.com>
2025-03-31 18:35:55 +0200

    Move Firebase config to .env and secure keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvfAWNCP2hPFDTSRgywBt_MQ7y06-_1kE",
-  authDomain: "last-hope-b1bf9.firebaseapp.com",
-  projectId: "last-hope-b1bf9",
-  storageBucket: "last-hope-b1bf9.firebasestorage.app",
-  messagingSenderId: "347036643244",
-  appId: "1:347036643244:web:66245c4c5591b64f2fd409"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
